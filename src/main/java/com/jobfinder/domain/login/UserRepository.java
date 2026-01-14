8c2152fe9d338e15a66d09cb2e88a489d86ed621
Jan-Pakos <janpakos123@gmail.com>
2026-01-14 13:31:43 +0100

    added mongorepository to UserRepository, updated InMemoryUserRepository

--- src/main/java/com/jobfinder/domain/login/UserRepository.java
+++ src/main/java/com/jobfinder/domain/login/UserRepository.java
@@ -1,12 +1,12 @@
 package com.jobfinder.domain.login;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends MongoRepository<User,String> {
 
     boolean existsByUsername(String username);
-
-    User save(User user);
 
     Optional<User> findByUsername(String username);
 }
