88ee96b69f51ad5dcc4f28cf05b7925fa005b91b
Jan-Pakos <janpakos123@gmail.com>
2026-01-26 17:23:45 +0100

    added cors for frontend access

--- src/integration/java/com/jobfinder/http/error/HttpRestTemplateIntegrationTestConfig.java
+++ src/integration/java/com/jobfinder/http/error/HttpRestTemplateIntegrationTestConfig.java
@@ -1,12 +1,12 @@
 package com.jobfinder.http.error;
 
 import com.jobfinder.domain.offer.OfferFetchable;
-import com.jobfinder.infrastructure.offer.http.OfferFetcherRestTemplateConfig;
+import com.jobfinder.infrastructure.offer.http.OfferFetcherConfig;
 import org.springframework.web.client.RestTemplate;
 
 import static com.jobfinder.BaseIntegrationTest.WIRE_MOCK_HOST;
 
-class HttpRestTemplateIntegrationTestConfig extends OfferFetcherRestTemplateConfig {
+class HttpRestTemplateIntegrationTestConfig extends OfferFetcherConfig {
 
     public OfferFetchable remoteOfferTest(int port, int connectionTimeout, int readTimeout) {
         final RestTemplate restTemplate = restTemplate(connectionTimeout, readTimeout, restTemplateResponseErrorHandler());
